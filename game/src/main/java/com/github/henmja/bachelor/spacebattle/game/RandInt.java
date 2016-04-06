1d35dbe18b956d46a2921623adb99b422e6065f0
henmja <hl.mjaaland@stud.uis.no>
2016-04-06 16:47:56 +0200

    Create RandInt.java

---
+++ game/src/main/java/com/github/henmja/bachelor/spacebattle/game/RandInt.java
@@ -0,0 +1,24 @@
+package com.github.henmja.bachelor.spacebattle.game;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+class RandInt {/* list with spawning intervals for the asteroid and the forcefield.*/
+	private List<Integer> intervalList = new ArrayList<>();
+
+	RandInt(int beg, int end) {
+		addInterval(beg, end);
+	}
+
+	int getRandom() {
+		return intervalList.get(new Random().nextInt(intervalList.size()));
+	}
+
+	public void addInterval(int beg, int end) {
+		for (int i = beg; i <= end; i++) {
+			intervalList.add(i);
+		}
+	}
+
+}
